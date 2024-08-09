07bc30ab9380fcec49b73dcf003672d2271b9981
Priyabrata Naskar <priyabrata.naskar@zomato.com>
2024-08-09 15:13:00 +0530

    removed logs

--- app/src/main/java/com/priyo/deeplinktesterpro/home/ui/view/DeeplinkItem.kt
+++ app/src/main/java/com/priyo/deeplinktesterpro/home/ui/view/DeeplinkItem.kt
@@ -1,6 +1,5 @@
 package com.priyo.deeplinktesterpro.home.ui.view
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -60,7 +59,6 @@ fun DeeplinkItem(
             modifier = Modifier
                 .clickable {
                     onDeleteItem()
-                    Log.e("tag", "clicked") //todo: remove logs
                 }
                 .fillMaxHeight()
                 .padding(vertical = 16.dp)
